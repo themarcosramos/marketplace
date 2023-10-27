b6e1b23c6a3bf2c303a8070335024c0e17c381c1
Marcos Ramos <mjramosfsa@gmail.com>
2023-10-27 16:40:17 -0300

    patch

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -8,5 +8,5 @@ define('DB_TYPE', 'pgsql');
 define('DB_HOST', 'localhost');
 define('DB_PORT', '5432'); 
 define('DB_NAME', '');
-define('DB_USER', 'postgres'); 
+define('DB_USER', ''); 
 define('DB_PASS', '');
